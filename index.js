aaaec5adb80f5cc672675a677307c0f46b686d45
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-12 19:44:31 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Gitau";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
